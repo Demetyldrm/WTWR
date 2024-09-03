5416b7f8c37685802d726c3af26da58ac1a02182
dyildi1 <demet.yildirim1@gmail.com>
2024-09-03 16:41:10 -0400

    first submit

---
+++ vite-project/src/components/App/App.jsx
@@ -0,0 +1,164 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { coordinates, APIkey } from "../../utils/constants";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/ItemModal";
+import { getWeather, filterWeatherData } from "../../utils/weatherApi";
+
+function App() {
+  const [weatherData, setWeatherData] = useState({
+    type: "",
+    temp: { F: 999, C: 999 },
+    city: "",
+  });
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const [selectedWeatherType, setSelectedWeatherType] = useState("");
+
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
+  const handleRadioChange = (event) => {
+    setSelectedWeatherType(event.target.id); // Set the selected radio button ID
+  };
+
+  useEffect(() => {
+    getWeather(coordinates, APIkey)
+      .then((data) => {
+        const filteredData = filterWeatherData(data);
+        setWeatherData(filteredData);
+      })
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    function handleCloseMethods(evt) {
+      if (evt.key === "Escape" || evt.key === "esc" || evt.keyCode === 27) {
+        closeActiveModal();
+      }
+
+      if (evt.type === "click" && evt.target.classList.contains("modal")) {
+        closeActiveModal();
+      }
+    }
+
+    if (activeModal !== "") {
+      document.addEventListener("keydown", handleCloseMethods);
+      document.addEventListener("click", handleCloseMethods);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleCloseMethods);
+      document.removeEventListener("click", handleCloseMethods);
+    };
+  }, [activeModal]);
+
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+        <Footer />
+      </div>
+      <ModalWithForm
+        title="New garment"
+        buttonText="Add garment"
+        activeModal={activeModal}
+        onClose={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label
+            htmlFor="Hot"
+            className={`modal__label modal__label_type_radio ${
+              selectedWeatherType === "hot"
+                ? "modal__label_type_radio--selected"
+                : ""
+            }`}
+          >
+            <input
+              id="hot"
+              type="radio"
+              className="modal__radio-input"
+              checked={selectedWeatherType === "hot"}
+              onChange={handleRadioChange}
+            />
+            Hot
+          </label>
+          <label
+            htmlFor="Warm"
+            className={`modal__label modal__label_type_radio ${
+              selectedWeatherType === "warm"
+                ? "modal__label_type_radio--selected"
+                : ""
+            }`}
+          >
+            <input
+              id="warm"
+              type="radio"
+              className="modal__radio-input"
+              checked={selectedWeatherType === "warm"}
+              onChange={handleRadioChange}
+            />
+            Warm
+          </label>
+          <label
+            htmlFor="Cold"
+            className={`modal__label modal__label_type_radio ${
+              selectedWeatherType === "cold"
+                ? "modal__label_type_radio--selected"
+                : ""
+            }`}
+          >
+            <input
+              id="cold"
+              type="radio"
+              className="modal__radio-input"
+              checked={selectedWeatherType === "cold"}
+              onChange={handleRadioChange}
+            />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
+      <ItemModal
+        activeModal={activeModal}
+        card={selectedCard}
+        onClose={closeActiveModal}
+      />
+    </div>
+  );
+}
+
+export default App;
